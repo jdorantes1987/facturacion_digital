ae03916699a57a4067e8f0c31b2fff8a370b14b0
jdorantes <165170008+jdorantes1987@users.noreply.github.com>
2025-06-06 17:02:33 -0400

    Initial commit

---
+++ articulos_profit.py
@@ -0,0 +1,33 @@
+import sys
+
+sys.path.append("..\\profit")
+from data.mod.inventario.articulos import Articulos
+
+
+class ArticulosProfit:
+    def __init__(self, conexion):
+        self.articulos = Articulos(conexion)
+
+    def get_articulos(self):
+        return self.articulos.get_articulos()
+
+
+if __name__ == "__main__":
+    import os
+
+    from conn.conexion import DatabaseConnector
+    from dotenv import load_dotenv
+
+    load_dotenv()
+    # Para SQL Server
+    datos_conexion = dict(
+        host=os.environ["HOST_PRODUCCION_PROFIT"],
+        base_de_datos=os.environ["DB_NAME_DERECHA_PROFIT"],
+    )
+    try:
+        oConexion = DatabaseConnector(db_type="sqlserver", **datos_conexion)
+        oArticulosProfit = ArticulosProfit(oConexion)
+        articulos = oArticulosProfit.get_articulos()
+        print(articulos)
+    except Exception as e:
+        print(f"Error al obtener articulos: {e}")
---
+++ clientes_profit.py
@@ -0,0 +1,33 @@
+import sys
+
+sys.path.append("..\\profit")
+from data.mod.ventas.clientes import Clientes
+
+
+class ClientesProfit:
+    def __init__(self, conexion):
+        self.clientes = Clientes(conexion)
+
+    def get_clientes(self):
+        return self.clientes.get_clientes_profit()
+
+
+if __name__ == "__main__":
+    import os
+
+    from conn.conexion import DatabaseConnector
+    from dotenv import load_dotenv
+
+    load_dotenv()
+    # Para SQL Server
+    datos_conexion = dict(
+        host=os.environ["HOST_PRODUCCION_PROFIT"],
+        base_de_datos=os.environ["DB_NAME_DERECHA_PROFIT"],
+    )
+    try:
+        oConexion = DatabaseConnector(db_type="sqlserver", **datos_conexion)
+        oClientesProfit = ClientesProfit(oConexion)
+        clientes = oClientesProfit.get_clientes()
+        print(clientes)
+    except Exception as e:
+        print(f"Error al obtener clientes: {e}")
