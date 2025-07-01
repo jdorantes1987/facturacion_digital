6430716cfbbfb9b573e9185e7de7dc4f281d4f5f
jdorantes <165170008+jdorantes1987@users.noreply.github.com>
2025-07-01 17:25:53 -0400

    Adaptaciones

--- sincroniza_facturacion.py
+++ sincroniza_facturacion.py
@@ -1,17 +1,24 @@
 import sys
 
+from pandas import DataFrame
+from numpy import nan
+
 from get_api_invoices import GetInvoices
 
 sys.path.append("..\\profit")
 from data.mod.ventas import documentos
+from data.mod.ventas.facturas_ventas import FacturasVentas
+from data.mod.ventas.clientes import Clientes
 
 
 class SincronizaFacturacion:
     def __init__(self, conexion, api_gateway_client):
         self.oDocumentos = documentos.Documentos(conexion)
+        self.oVentas = FacturasVentas(conexion)
+        self.oClientes = Clientes(conexion)
         self.client = api_gateway_client
 
-    def set_facturas_profit(self, params=None):
+    def __set_facturas_profit(self, params=None):
         documentos = self.oDocumentos.get_documentos(tipo_doc="FACT")
         # fecha sin hora, minutos y segundos
         documentos["fec_emis"] = documentos["fec_emis"].dt.normalize()
@@ -21,22 +28,59 @@ class SincronizaFacturacion:
         ]
         return set(documentos["nro_doc"].str.strip())
 
-    def set_facturas_imprenta(self, params=None):
+    def __set_facturas_imprenta(self, params=None):
         oGetInvoices = GetInvoices(self.client)
         facturas_imprenta = oGetInvoices.get_data_invoices(params=params)
         if facturas_imprenta.empty:
             return set()
         return set(facturas_imprenta["invoice_number"].str.strip())
 
-    def diferencias_profit_vs_imprenta(self, params=None):
-        set_profit = self.set_facturas_profit(params=params)
-        set_imprenta = self.set_facturas_imprenta(params=params)
+    def __diferencias_profit_vs_imprenta(self, params=None):
+        set_profit = self.__set_facturas_profit(params=params)
+        set_imprenta = self.__set_facturas_imprenta(params=params)
         return set_profit.difference(set_imprenta)
 
-    def diferencias_imprenta_vs_profit(self, params=None):
-        set_profit = self.set_facturas_profit(params=params)
-        set_imprenta = self.set_facturas_imprenta(params=params)
+    def __diferencias_imprenta_vs_profit(self, params=None):
+        set_profit = self.__set_facturas_profit(params=params)
+        set_imprenta = self.__set_facturas_imprenta(params=params)
         return set_imprenta.difference(set_profit)
+
+    def data_a_validar_en_sheet(self, params=None) -> DataFrame:
+        # Campos a validar en Sheet
+        campos_a_validar = [
+            "doc_num_encab",
+            "rif",
+            "fec_emis",
+            "co_art",
+            "total_art",
+            "reng_neto",
+            "comentario",
+        ]
+        # Conjuntos de facturas de Profit e Imprenta
+        set_profit = self.__set_facturas_profit(params=params)
+        set_imprenta = self.__set_facturas_imprenta(params=params)
+        # Obtener las diferencias entre Profit e Imprenta
+        diferencias = set_profit.difference(set_imprenta)
+        # Si no hay diferencias, retornar un DataFrame vacío
+        if not diferencias:
+            return DataFrame()
+
+        # Obtener los clientes de Profit
+        clientes = self.oClientes.get_clientes_profit()[["co_cli", "rif"]]
+        # Obtener las facturas de Profit entre las fechas especificadas
+        data = self.oVentas.get_facturas(
+            fecha_d=params["fechaInicio"], fecha_h=params["fechaFin"]
+        )
+        data["co_cli"] = data["co_cli"].str.strip()
+        data["doc_num_encab"] = data["doc_num_encab"].str.strip()
+        data["co_art"] = data["co_art"].str.strip()
+        data.sort_values(by=["doc_num_encab", "reng_num"], inplace=True)
+        # Combinar datos de facturas con clientes de Profit
+        data = data.merge(clientes, left_on="co_cli", right_on="co_cli", how="left")
+        # Filtrar las facturas que no están en la imprenta
+        data = data[data["doc_num_encab"].isin(diferencias)]
+        # Reemplazar valores NaN por cadenas vacías
+        return data.replace(nan, "", regex=True)[campos_a_validar]
 
 
 if __name__ == "__main__":
@@ -74,5 +118,9 @@ if __name__ == "__main__":
         "fechaInicio": "2025-06-20",  # Fecha de inicio del rango
         "fechaFin": hoy,  # Fecha de fin del rango
     }
-    # print(oSincronizaFacturacion.diferencias_imprenta_vs_profit(params=params))
-    print(oSincronizaFacturacion.set_facturas_profit(params=params))
+    print(
+        oSincronizaFacturacion.data_a_validar_en_sheet(params=params).to_string(
+            index=False
+        )
+    )
+    # print(oSincronizaFacturacion.set_facturas_profit(params=params))
