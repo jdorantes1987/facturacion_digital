baf5a9659c9e20b12003b3ea4250bb9f8b04c6e0
jdorantes <165170008+jdorantes1987@users.noreply.github.com>
2025-07-14 09:33:32 -0400

    Implementación logging.config con archivo "logging.ini"

--- add_products.py
+++ add_products.py
@@ -1,12 +1,13 @@
 import logging
+import logging.config
 
-from api_gateway_client import ApiGatewayClient
-from api_key_manager import ApiKeyManager
+logging.config.fileConfig("logging.ini")
 
 
 class AddProducts:
     def __init__(self, api_gateway_client):
         self.client = api_gateway_client
+        self.logger = logging.getLogger(__class__.__name__)
 
     def add_product(self, product_data) -> dict:
         try:
@@ -19,9 +20,10 @@ class AddProducts:
                     "Invalid product_data format. Expected a dictionary with a 'productos' key containing a list."
                 )
             response = self.client.post_data(product_data)
+            self.logger.info("Respuesta POST: %s", response)
             return response
         except Exception as e:
-            logging.error("Error al agregar producto: %s", e)
+            self.logger.error(f"Error al agregar producto: %s {e}", exc_info=True)
             return {"error": str(e)}
 
 
@@ -30,24 +32,22 @@ if __name__ == "__main__":
 
     from dotenv import load_dotenv
 
+    from api_gateway_client import ApiGatewayClient
+    from api_key_manager import ApiKeyManager
     from data_facturacion import DataFacturacion
 
     load_dotenv(override=True)
-    logging.basicConfig(
-        level=logging.ERROR, format="%(asctime)s - %(levelname)s - %(message)s"
-    )
 
     FILE_FACTURACION_NAME = os.getenv("GOOGLE_SHEET_FILE_FACTURACION_NAME")
     SPREADSHEET_ID = os.getenv("GOOGLE_SHEET_FACTURACION_ID")
-    SHEET_NAME = os.getenv("GOOGLE_SHEET_FACTURACION_NAME")
     CREDENTIALS_FILE = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
+
+    oDataFacturacion = DataFacturacion(
+        SPREADSHEET_ID, FILE_FACTURACION_NAME, CREDENTIALS_FILE
+    )
 
     oProducts = AddProducts(
         ApiGatewayClient(os.getenv("API_GATEWAY_URL_PRODUCTS"), ApiKeyManager())
-    )
-
-    oDataFacturacion = DataFacturacion(
-        FILE_FACTURACION_NAME, SPREADSHEET_ID, SHEET_NAME, CREDENTIALS_FILE
     )
 
     # Ejemplo de POST agregando un producto
@@ -57,6 +57,6 @@ if __name__ == "__main__":
         # Asignar la clave "productos" al dict
         payload = {"productos": productos}
         result = oProducts.add_product(payload)  # Envía el dict, no el string
-        print("Respuesta POST:", result)
+        # print("Respuesta POST:", result)
     except Exception as e:
         print("Error en POST:", e)
